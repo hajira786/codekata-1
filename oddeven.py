6c49fd5179b4ce0f518590c83e1900479b40ecb9
hajira786 <47344240+hajira786@users.noreply.github.com>
2019-02-05 12:04:43 +0530

    Update oddeven.py

--- oddeven.py
+++ oddeven.py
@@ -1,7 +1,7 @@
 print("CHECK WHETHER THE GIVEN NO IS EVEN OR ODD")
-a = input()
-a = int(a)
-if(a%2==0):
+x = input()
+x = int(x)
+if(x%2==0):
  print("The Given Number is Even")
 else:
  print("The Given Number is Odd")
