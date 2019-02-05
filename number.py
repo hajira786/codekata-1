7da8301ea6d5c27402e6ec121d5c1f342005fd21
hajira786 <47344240+hajira786@users.noreply.github.com>
2019-02-05 11:53:29 +0530

    Update number.py

--- number.py
+++ number.py
@@ -1,9 +1,9 @@
 print("Check The Number")
-x = input()
-x = int(x)
-if(x > 0):
+h = input()
+h = int(h)
+if(h > 0):
   print("Positive")
-elif(x < 0):
+elif(h < 0):
   print("Negative")
 else:
   print("Zero")
